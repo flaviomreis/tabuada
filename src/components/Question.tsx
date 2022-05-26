dd7a6d81c0d6bb4efe092df8fe0767620b348e53
Flavio Menezes dos Reis <flaviomreis@gmail.com>
2022-05-26 11:11:45 -0300

    add fix question in Question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -17,7 +17,21 @@ function Question() {
 
   const [tabuada, setTabuada] = useState(itensTabuada);
 
-  return <div>{itensTabuada.length}</div>;
+  return (
+    <div className="flex gap-2">
+      <span className="px-2 h-10 rounded bg-cyan-700 text-slate-300">
+        {itensTabuada[15].value1}
+      </span>
+      <span className="px-2 h-10 rounded text-cyan-700">x</span>
+      <span className="px-2 h-10 rounded bg-cyan-700 text-slate-300">
+        {itensTabuada[15].value2}
+      </span>
+      <span className="px-2 h-10 text-cyan-700">=</span>
+      <span className="px-2 h-10 bg-cyan-700 text-slate-300">
+        {itensTabuada[15].result}
+      </span>
+    </div>
+  );
 }
 
 export default Question;
